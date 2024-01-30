569315c84d97813375153aca2242153252046ba8
Clemente <jopclem@gmail.com>
2024-01-30 15:45:34 +0000

    Add test database name to config

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     database_password: str
     database_name: str
     database_username: str
+    test_database_name: str
 
     jwt_secret_key: str
     jwt_algorythm: str
