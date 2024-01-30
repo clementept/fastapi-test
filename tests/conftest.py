2e777be915d163fa48eb26541617ea1c0c6795eb
Clemente <jopclem@gmail.com>
2024-01-30 16:10:46 +0000

    Add conftest file with fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,12 +6,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
 from alembic import command
+import pytest
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.test_database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture
+def test_user(client):
+    res = client.post("/users", json={"email": "hello@mail.com", "password": "123"})
+
+    new_user = res.json()
+    new_user["password"] = "123"
+
+    return new_user
 
 
 @pytest.fixture
@@ -23,6 +34,7 @@ def session():
         yield db
     finally:
         db.close()
+
 
 @pytest.fixture
 def client(session):
